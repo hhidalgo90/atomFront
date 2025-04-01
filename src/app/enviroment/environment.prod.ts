9b5125690694a6c59dc8cbfc61a9af179f52cb30
hhidalgo90 <hector.hidalgo.bbt@gmail.com>
2025-04-01 19:49:37 -0300

    Front v2, prod environment

---
+++ src/app/enviroment/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production    : false,
+    version       : "1.0.0",
+    
+    apiUserUrl  : "https://atombackendservice-44850752985.us-central1.run.app/api/users",
+    apiTaskUrl  : "https://atombackendservice-44850752985.us-central1.run.app/api/tasks",
+
+  };
